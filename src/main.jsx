d38abf7aa150bdb1ecea2770aa387e96a262ba21
zionzzzz <zion970504@gmail.com>
2026-05-01 03:19:20 +0800

    徹底消滅 TS

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 // @ts-ignore   👈 就是這行！這代表「糾察隊請閉嘴，不要檢查下一行」
 import App from './App.jsx'
 import './index.css'
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>
