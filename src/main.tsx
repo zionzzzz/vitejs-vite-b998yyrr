62eb073bad40fcd4ecca961afde663e77b951e97
zionzzzz <zion970504@gmail.com>
2026-05-01 00:36:07 +0800

    加上隱形斗篷

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// @ts-ignore   👈 就是這行！這代表「糾察隊請閉嘴，不要檢查下一行」
+import App from './App.jsx'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
