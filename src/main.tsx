f79709765dd51619e0874e93e3a5f64a075d6cb5
zionzzzz <zion970504@gmail.com>
2026-05-01 03:08:30 +0800

    發假身分證

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 // @ts-ignore   👈 就是這行！這代表「糾察隊請閉嘴，不要檢查下一行」
 import App from './App.jsx'
 import './index.css'
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
